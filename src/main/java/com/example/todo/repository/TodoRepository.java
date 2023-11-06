e42b2ec2f9d32d5eb4e00a6e6c13f550645e0b10
Jasur <jasurfozilov42@gmail.com>
2023-11-06 20:32:37 +0500

    first commit

---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.example.todo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.todo.model.Todo;
+
+public interface TodoRepository extends JpaRepository <Todo, Long> {
+    List<Todo> findByUserName(String userName);
+    
+}
